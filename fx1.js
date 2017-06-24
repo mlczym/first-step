a6f07e22ca288aa3316cc8dec65aa305ddb5111b
mlczym <nzy19941994@outlook.com>
2017-06-24 14:25:27 +0800

    Add files via upload

---
+++ fx1.js
@@ -0,0 +1,9 @@
+    function showtext(){
+ 	        var mylist=document.getElementById('lg-list-drop');
+            mylist.style.display='block';
+        }
+    function hidetext(){
+            var mylist=document.getElementById('lg-list-drop');
+            mylist.style.display='none';
+        }
+
